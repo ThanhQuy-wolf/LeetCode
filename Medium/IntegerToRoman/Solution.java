312073f8a012ba98902afa6580b2f7459a903419
Tạ Nguyễn Thành Quý <524h0124@student.tdtu.edu.vn>
2025-10-29 07:40:37 +0700

    Integer to Roman

---
+++ Medium/IntegerToRoman/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public static String intToRoman(int num) {
+        if (num < 1 || num > 3999)
+            return "";
+        
+        StringBuilder result = new StringBuilder();
+
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                num -= values[i];
+                result.append(symbols[i]);
+            }
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(intToRoman(2350));
+    }
+}
